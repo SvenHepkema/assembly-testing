ce3606f1a13bf2acd7645f7f00db922f3f266c66
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2024-02-24 11:42:57 +0100

    Annotated the hello world assembly and improved readability

--- assembly/hello_world.asm
+++ assembly/hello_world.asm
@@ -1,14 +1,13 @@
-# Copied from https://stackoverflow.com/questions/13995906/get-filename-without-extension-within-makefile
 section     .text
 global      _start 
 _start: 
-    mov     edx,len   
-    mov     ecx,msg   
-    mov     ebx,1   
-    mov     eax,4   
-    int     0x80   
-    mov     eax,1  
-    int     0x80   
+    mov     eax,4			; __NR_write from asm/unistd_64.h (https://github.com/ilbers/linux/blob/master/arch/sh/include/uapi/asm/unistd_64.h)
+    mov     ebx,1			; stdout fileno
+    mov     ecx,msg		; Point c to message
+    mov     edx,len		; Point d to length
+    int     0x80			; Interrupt: execute systemcall
+    mov     eax,1			; __NR_exit
+    int     0x80			; Interrupt: execute systemcall
 section     .data
-msg     db  'Hello world',0xa  
+msg     db  'Hello world!',0xa  
 len     equ $ - msg   
